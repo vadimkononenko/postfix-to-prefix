ac82e679dbcb3b64c7e889df0a29e3fe261db78c
rogosnyi <lesha3320@gmail.com>
2022-06-19 17:13:05 +0300

    feature: implementation.go

--- implementation.go
+++ implementation.go
@@ -1,9 +1,34 @@
 package lab2
 
-import "fmt"
+import (
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func isOperator(element string) bool {
+	if element == "*" || element == "/" || element == "+" || element == "-" || element == "^" {
+		return true
+	} else {
+		return false
+	}
+}
+
+func PostfixToPrefix(input string) (string, error) {
+	postfixArray := strings.Split(input, " ")
+
+	stack := make([]string, 0)
+
+	for i := 0; i < len(postfixArray); i-- {
+		if isOperator(postfixArray[i]) {
+			op1 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			op2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			temp := postfixArray[i] + op2 + op1
+			stack = append(stack, temp)
+		} else {
+			stack = append(stack, string(postfixArray[i]))
+		}
+	}
+	return stack[0], nil
 }
--- implementation_test.go
+++ implementation_test.go
@@ -7,15 +7,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
+func TestPostfixToPrefix(t *testing.T) {
+	res, err := PostfixToPrefix("+ 5 * - 4 2 3")
 	if assert.Nil(t, err) {
 		assert.Equal(t, "4 2 - 3 * 5 +", res)
 	}
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+func ExamplePostfixToPrefix() {
+	res, _ := PostfixToPrefix("+ 2 2")
 	fmt.Println(res)
 
 	// Output:
