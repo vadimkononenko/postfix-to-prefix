80301ad7b92d723b40dad75391cf16f23614732c
rogosnyi <lesha3320@gmail.com>
2022-06-19 18:55:59 +0300

    fix: implementation.go, feature: implementation_test.go

--- implementation.go
+++ implementation.go
@@ -1,34 +1,58 @@
 package lab2
 
 import (
+	"errors"
 	"strings"
 )
-
-func isOperator(element string) bool {
-	if element == "*" || element == "/" || element == "+" || element == "-" || element == "^" {
-		return true
-	} else {
-		return false
-	}
-}
 
 func PostfixToPrefix(input string) (string, error) {
 	postfixArray := strings.Split(input, " ")
 
 	stack := make([]string, 0)
 
-	for i := 0; i < len(postfixArray); i-- {
-		if isOperator(postfixArray[i]) {
-			op1 := stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
-			op2 := stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
-
-			temp := postfixArray[i] + op2 + op1
-			stack = append(stack, temp)
+	var operatorCount int
+	var operandCount int
+	for _, element := range postfixArray {
+		if element == "+" || element == "-" || element == "*" || element == "/" || element == "^" {
+			operatorCount++
 		} else {
-			stack = append(stack, string(postfixArray[i]))
+			operandCount++
 		}
 	}
+
+	if operatorCount != operandCount-1 {
+		return "", errors.New("invalid prefix equation")
+	}
+
+	for i := 0; i <= len(postfixArray)-1; i++ {
+		if postfixArray[i] != "+" && postfixArray[i] != "-" && postfixArray[i] != "*" && postfixArray[i] != "/" && postfixArray[i] != "^" {
+			stack = append(stack, postfixArray[i])
+		} else {
+			if len(stack) >= 2 {
+				num1 := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				num2 := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				stack = append(stack, postfixArray[i]+num2+num1)
+			} else {
+				return "", errors.New("invalid prefix equation")
+			}
+		}
+	}
+
+	//for i := 0; i <= len(postfixArray)-1; i++ {
+	// if isOperator(postfixArray[i]) {
+	//  op1 := stack[len(stack)-1]
+	//  stack = stack[:len(stack)-1]
+	//  op2 := stack[len(stack)-1]
+	//  stack = stack[:len(stack)-1]
+	//
+	//  temp := postfixArray[i] + op2 + op1
+	//  stack = append(stack, temp)
+	// } else {
+	//  stack = append(stack, postfixArray[i])
+	// }
+	//}
+
 	return stack[0], nil
 }
--- implementation_test.go
+++ implementation_test.go
@@ -1,23 +1,69 @@
 package lab2
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPostfixToPrefix(t *testing.T) {
-	res, err := PostfixToPrefix("+ 5 * - 4 2 3")
+func TestFirst(t *testing.T) {
+	res, err := PostfixToPrefix("1 2 + 3 4 + *")
 	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+		assert.Equal(t, "*+12+34", res)
 	}
 }
 
-func ExamplePostfixToPrefix() {
-	res, _ := PostfixToPrefix("+ 2 2")
-	fmt.Println(res)
+func TestSecond(t *testing.T) {
+	res, err := PostfixToPrefix("7 2 ^ 4 9 - 12 * +")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "+^72*-4912", res)
+	}
+}
 
-	// Output:
-	// 2 2 +
+func TestThird(t *testing.T) {
+	res, err := PostfixToPrefix("12 6 4 + 2 ^ - 5 +")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "+-12^+6425", res)
+	}
+}
+
+func TestFourth(t *testing.T) {
+	res, err := PostfixToPrefix("x y ^ 5 s * / 10 +")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "+/^xy*5s10", res)
+	}
+}
+
+func TestFifth(t *testing.T) {
+	res, err := PostfixToPrefix("x y ^ 5 s * / 10 +")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "+/^xy*5s10", res)
+	}
+}
+
+func TestSix(t *testing.T) {
+	res, err := PostfixToPrefix("4 3 ^ 6 11 - 2 ^ - 1 +")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "+-^43^-61121", res)
+	}
+}
+
+func TestErrorFirst(t *testing.T) {
+	_, err := PostfixToPrefix("$ 1")
+	assert.NotNil(t, err)
+}
+
+func TestErrorSecond(t *testing.T) {
+	_, err := PostfixToPrefix("- - + 3 - 4")
+	assert.NotNil(t, err)
+}
+
+func TestErrorThird(t *testing.T) {
+	_, err := PostfixToPrefix(" ")
+	assert.NotNil(t, err)
+}
+
+func TestErrorFourth(t *testing.T) {
+	_, err := PostfixToPrefix("1 2 3 4 5 6 7 8")
+	assert.NotNil(t, err)
 }
